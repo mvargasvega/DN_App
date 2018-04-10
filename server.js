5725e41e1d290562787bbc82bb64fccba3b02f85
mvargasvega <martin@digitalnest.org>
2018-04-09 20:09:11 -0700

    Server configuration

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+//Configure app
+app.set('views', __dirname + 'views');      // Views directory
+app.use(express.static('public'));          // Static directory
+app.use(bodyParser.urlencoded({ extended: true })); // req.body
+
+// Set CORS Headers
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Homepage -- Display a list of current todos and a form
+app.get('/', function(req, res) {
+    res.sendFile( __dirname + '/views/index.html')
+});
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000...")
+})
